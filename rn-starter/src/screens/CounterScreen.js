1809e306a2dbfb1a0b6f6d6ec2dec010b76205dc
jhatting <jarrodhatting@icloud.com>
2020-05-16 12:22:06 -0700

    update

---
+++ rn-starter/src/screens/CounterScreen.js
@@ -0,0 +1,13 @@
+import react from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+const CounterScreen = () => {
+    return (<View>
+        <Text>Current Count:</Text>
+    </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default CounterScreen;
